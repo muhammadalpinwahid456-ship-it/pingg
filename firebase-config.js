1de3fce17b598e97c5e4f4dd0229c2663ad59380
muhammadalpinwahid456-ship-it <muhammadalpinwahid456@gmail.com>
2026-05-10 09:10:43 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDz6Lo5LoYRmVpHL9egJUdiZosBBNWa2aA",
-  authDomain: "alvin-f573e.firebaseapp.com",
-  databaseURL: "https://alvin-f573e-default-rtdb.firebaseio.com",
-  projectId: "alvin-f573e",
-  storageBucket: "alvin-f573e.firebasestorage.app",
-  messagingSenderId: "718704784181",
-  appId: "1:718704784181:web:6bcdf70a5d35bcd875d27c",
-  measurementId: "G-T7SW3DC8YC"
+   apiKey: "AIzaSyDzDRaHEASBWO0dDKOIlvlku3lN01b89iw",
+    authDomain: "alvin-f573e.firebaseapp.com",
+    projectId: "alvin-f573e",
+    storageBucket: "alvin-f573e.firebasestorage.app",
+    messagingSenderId: "718704784181",
+    appId: "1:718704784181:web:db5fe912a3cb3b0675d27c",
+    measurementId: "G-NHLWBMV307"
 };
 
 // Initialize Firebase
